9836af916d4d35f4b9150de74b2cf81c86b9a4a6
ariadnaki <ariadnaruizcastillo@gmail.com>
2023-02-28 23:48:36 +0100

    HUD

--- DIVIDE ET IMPERA/Assets/Scripts/UIManager.cs
+++ DIVIDE ET IMPERA/Assets/Scripts/UIManager.cs
@@ -1,16 +1,44 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    public static PlayerManager.TimmyStates UIState;
 
+    [SerializeField] private static GameObject _craneoTorsoNO;
+    [SerializeField] private static GameObject _legsNO;
+    [SerializeField] private static GameObject _armsNO;
+
+    #region methods
+    static public void UpdateHud(PlayerManager.TimmyStates UIState)
+    {
+        if(UIState == PlayerManager.TimmyStates.S1 || UIState == PlayerManager.TimmyStates.S2)
+        {
+            _armsNO.SetActive(true);
+            _legsNO.SetActive(false);
+            _craneoTorsoNO.SetActive(false);
+        }
+        else if(UIState == PlayerManager.TimmyStates.S3)
+        {
+            _legsNO.SetActive(true);
+            _armsNO.SetActive(false);
+            _craneoTorsoNO.SetActive(false);
+        }
+        else if (UIState == PlayerManager.TimmyStates.S4)
+        {
+            _armsNO.SetActive(true);
+            _legsNO.SetActive(true);
+            _craneoTorsoNO.SetActive(false);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
 
+    #endregion
+    void Start()
+    {
+        _armsNO.SetActive(false);
+        _legsNO.SetActive(false);
+        _craneoTorsoNO.SetActive(false);
     }
 }
