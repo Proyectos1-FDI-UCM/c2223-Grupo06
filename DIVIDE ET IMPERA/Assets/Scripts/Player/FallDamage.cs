fc1ed844f157579628be0dcf8ffb38db444846a6
Usuario Local <123581035+nievesag@users.noreply.github.com>
2023-03-01 10:37:33 +0100

    daño de caída usando la altura WIP

--- DIVIDE ET IMPERA/Assets/Scripts/Player/FallDamage.cs
+++ DIVIDE ET IMPERA/Assets/Scripts/Player/FallDamage.cs
@@ -12,6 +12,8 @@ public class FallDamage : MonoBehaviour
     private float _allowedSpeed; // velocidad máx que permito antes de hacer daño
     [SerializeField]
     private float _previousSpeed; // velocidad antes de llegar al suelo
+    //[SerializeField]
+    //private float _allowedHeight = ;
     #endregion
     #region properties
     [SerializeField]
@@ -29,7 +31,8 @@ public class FallDamage : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        float _height = (((_rigidBody2D.velocity.y) * (_rigidBody2D.velocity.y)) / (2 * Physics2D.gravity.y));
+        Debug.Log(Physics2D.gravity);
         // a quien vea esto: me estoy volviendo completamente loca
         /*_onGround = _groundCheck._isGrounded; // comprobación de _onGround
         if (!_onGround)
@@ -47,11 +50,19 @@ public class FallDamage : MonoBehaviour
         }*/
 
         // _onGround = _groundCheck._isGrounded; // comprobación de _onGround
+
+
+        /* if (_height < _allowedHeight) // si se supera la altura permitida y se está en el suelo -> aplicas daño
+         {
+             _boneStateBar.BoneDamage(_damage: 20f);
+         }*/
+
+        /*
         if (_rigidBody2D.velocity.y < _allowedSpeed) // si se supera la velocidad permitida y se está en el suelo -> aplicas daño
         {
             Debug.Log(_rigidBody2D.velocity.y);
             _boneStateBar.BoneDamage(_damage: 20f);
             //_previousSpeed = 0; // si se llega al suelo la velocidad vuelve a 0
-        }
+        }*/
     }
 }
