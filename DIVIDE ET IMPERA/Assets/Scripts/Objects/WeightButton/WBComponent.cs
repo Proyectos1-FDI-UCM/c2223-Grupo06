52547fabf4308c6b97682a707cc1a54a81b44fd4
ariadnaki <ariadr02@ucm.es>
2023-03-04 12:27:06 +0100

    Weight and button

--- DIVIDE ET IMPERA/Assets/Scripts/Objects/WeightButton/WBComponent.cs
+++ DIVIDE ET IMPERA/Assets/Scripts/Objects/WeightButton/WBComponent.cs
@@ -4,35 +4,64 @@ using UnityEngine;
 
 public class WBComponent : MonoBehaviour
 {
-    [SerializeField] private GameObject _player;
+    #region references
     [SerializeField] private GameObject _puerta;
 
+    [SerializeField]
+    Transform pointA;
+    [SerializeField]
+    Transform pointB;
+    #endregion
+
+    #region Properties
+
+    Collider2D[] colliders;
+
+    #endregion
     private SpriteRenderer _mySpriteRenderer;
 
-    private void Activate()
+
+    /*
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        /*
-        if (Collision.GetComponent<WeightComponent>)
+        if (collision.gameObject.GetComponent<WeightComponent>() != null)
         {
-            _mySpriteRenderer.color= Color.white;
+            _mySpriteRenderer.color = Color.white;
+            _puerta.SetActive(false);
+        }
+        else 
+        {
+            _mySpriteRenderer.color = Color.magenta;
             _puerta.SetActive(true);
         }
-        */
     }
-
-
-
-
+    */
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        _mySpriteRenderer = GetComponent<SpriteRenderer>();
+        _mySpriteRenderer = transform.parent.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        colliders = Physics2D.OverlapAreaAll(pointA.position, pointB.position);
+
+        int i = 0;
+        while (i < colliders.Length && colliders[i].gameObject.GetComponent<WeightComponent>() == null) i++;
+
+        if(i != colliders.Length)
+        {
+            _mySpriteRenderer.color = Color.white;
+            _puerta.SetActive(false);
+        }
+        else
+        {
+            _mySpriteRenderer.color = Color.magenta;
+            _puerta.SetActive(true);
+        }
     }
 }
