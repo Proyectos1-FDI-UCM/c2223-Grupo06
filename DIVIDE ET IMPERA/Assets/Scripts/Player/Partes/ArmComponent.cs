1114ded8c39a371e985467690c2af39ca91a5fe9
David-104 <118064911+David-104@users.noreply.github.com>
2023-03-07 17:43:11 +0100

    Brazo se conecta a palancas al lanzarlo/soltarlo

--- DIVIDE ET IMPERA/Assets/Scripts/Player/Partes/ArmComponent.cs
+++ DIVIDE ET IMPERA/Assets/Scripts/Player/Partes/ArmComponent.cs
@@ -2,6 +2,20 @@ using UnityEngine;
 
 public class ArmComponent : MonoBehaviour
 {
+    #region References
+
+    #endregion
+    #region Parameters
+
+    #endregion
+    #region Properties
+    private Collider2D[] _colliders;
+    private bool _leverFound;
+    private GameObject _touchedLever;
+    #endregion
+    #region Methods
+    
+    #endregion
     // Start is called before the first frame update
     void Start()
     {
@@ -11,6 +25,26 @@ public class ArmComponent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        _colliders = Physics2D.OverlapCircleAll(transform.position, 1f);
+        int i = 0;
+        _leverFound = false;
 
+        while(i < _colliders.Length && !_leverFound)
+        {
+            if (_colliders[i].gameObject.GetComponent<PalancaComponent>() != null)
+            {
+                _leverFound = true;
+                _touchedLever = _colliders[i].gameObject;
+            }
+            else
+                i++;
+        }
+        if(_leverFound && !_touchedLever.GetComponent<PalancaComponent>().BrazoConectado)
+        {
+            _leverFound = false;
+            _touchedLever.GetComponent<PalancaComponent>().ConectarBrazo(true);
+            _touchedLever = null;
+            Destroy(gameObject);
+        }
     }
 }
