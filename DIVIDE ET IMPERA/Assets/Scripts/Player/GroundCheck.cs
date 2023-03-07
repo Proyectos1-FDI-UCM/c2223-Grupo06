fd64d6bb34ce9d9a1dfd591c9ad88d89f507438a
David-104 <118064911+David-104@users.noreply.github.com>
2023-03-07 18:55:36 +0100

    GroundCheck arregladooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo

--- DIVIDE ET IMPERA/Assets/Scripts/Player/GroundCheck.cs
+++ DIVIDE ET IMPERA/Assets/Scripts/Player/GroundCheck.cs
@@ -5,8 +5,11 @@ public class GroundCheck : MonoBehaviour
     #region Properties 
     public bool _isGrounded;
     public bool IsGrounded { get { return _isGrounded; } }
+
+    private LayerMask _levelMask;
     #endregion
     #region Methods
+    /*
     //Cuando los pies del jugador (o sea el Ground Check) toca el suelo
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -16,6 +19,24 @@ public class GroundCheck : MonoBehaviour
     private void OnTriggerExit2D(Collider2D collision)
     {
         _isGrounded = false;
-    }
+    }*/
     #endregion
+
+    private void Start()
+    {
+        _levelMask = LayerMask.GetMask("Level");
+    }
+
+    private void Update()
+    {
+        //Lanza raycast hacia abajo en tres puntos (izq, centro y drcha de Timmy, para no depender solo de uno de esos puntos)
+        if (Physics2D.Raycast(transform.position, Vector2.down, 1.5f, _levelMask)
+            || Physics2D.Raycast(transform.position - new Vector3(0.5f, 0f, 0f), Vector2.down, 1.5f, _levelMask)
+            || Physics2D.Raycast(transform.position + new Vector3(0.5f, 0f, 0f), Vector2.down, 1.5f, _levelMask))
+        {
+            _isGrounded = true;
+        }
+        else
+            _isGrounded= false;
+    }
 }
