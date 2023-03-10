9986b01a9187d7e1fb369cb78df4d516df5a2b44
cynthia <118978961+cyntrist@users.noreply.github.com>
2023-03-10 23:54:12 +0100

    empezando hud

--- DIVIDE ET IMPERA/Assets/Scripts/UI/UIManager.cs
+++ DIVIDE ET IMPERA/Assets/Scripts/UI/UIManager.cs
@@ -1,7 +1,18 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    #region references
+    [SerializeField] private Image _cabezaIMG;
+    [SerializeField] private Image _brazo1IMG;
+    [SerializeField] private Image _brazo2IMG;
+    [SerializeField] private Image _piernasIMG;
+    [SerializeField] private Image _costillasIMG;
+    [SerializeField] private Image _piernasAlubiatIMG;
+    [SerializeField] private Scrollbar _barra;
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
