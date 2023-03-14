5f05c00f1c68470ba60363190dac0953652b9194
Leves <nievalon@ucm.es>
2023-03-14 22:05:09 +0100

    mini cambios antes de cenar

--- DIVIDE ET IMPERA/Assets/Scripts/DialogueSystem/Dialogue.cs
+++ DIVIDE ET IMPERA/Assets/Scripts/DialogueSystem/Dialogue.cs
@@ -11,6 +11,8 @@ public class Dialogue
     #region Parameters
     // public enum Personajes { Timmy, Bob, Alubia, Narrador }; // 0 -> 3
 
+    public string _name;
+
     [TextArea(3, 10)]
     public string[] _parrafos; // parrafos que se irán cargando en la cola de queue
     #endregion
--- DIVIDE ET IMPERA/Assets/Scripts/DialogueSystem/DialogueTrigger.cs
+++ DIVIDE ET IMPERA/Assets/Scripts/DialogueSystem/DialogueTrigger.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour
 {
+    #region References
     public Dialogue _dialogue;
+    private DialogueManager _dialogueManager;
+    #endregion
+    #region Parameters
+    
+    #endregion
+    #region Properties
+
+    #endregion
+    #region Methods
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(_dialogue);
+    }
+    # endregion
+    
 }
