6a08c2ef878c8dd30775d71922b43ec33fd6fe44
David-104 <118064911+David-104@users.noreply.github.com>
2023-03-25 20:41:26 +0100

    Codigo comentado porque estoy bobo y se me olvido hacerlo antes de pushear

--- DIVIDE ET IMPERA/Assets/Scripts/Camera/RoomTransition.cs
+++ DIVIDE ET IMPERA/Assets/Scripts/Camera/RoomTransition.cs
@@ -4,55 +4,64 @@ using UnityEngine;
 public class RoomTransition : MonoBehaviour
 {
     #region References
+    //Transforms camara y jugador
     private Transform _cameraTransform;
     private Transform _player;
+
+    //Aqui empieza lo bueno
     [SerializeField]
-    private Transform _roomCameraPosition;
+    private Transform _roomCameraPosition; //Posicion a la que queremos que la camara se mueva durante la transicion
     [SerializeField]
-    private Transform _roomSpawn;
+    private Transform _roomSpawn; //Lugar al que el jugador se movera tras la transicion (ademas servira para spawnearle ahi si quiere resetear la sala)
     #endregion
     #region Parameters
     [SerializeField]
-    private float _transitionSpeed;
+    private float _transitionSpeed; //Velocidad de transicion de la camara, valor alto para que no se note lo que ocurre durante la transicion
     #endregion
     #region Properties
-    private Vector3 _futureCamPos;
-    private bool _onTransition;
-    GameObject[] _transitions;
+    private Vector3 _futureCamPos; //futura posicion de la camara
+    private bool _onTransition; //booleano que determina si esta ocurrinedo una transicion
+    GameObject[] _transitions; //Array de todas las transiciones del juego (durante el transcurso de una transicion se desactivan para evitar bugs, antes se rompia todo si entrabas en una transicion estando ya en una)
     #endregion
     #region Methods
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision) //Cuando entras en la transicion
     {
-        CameraMovement.Instance.enabled = false;
-        _player.transform.position = _roomSpawn.position;
+        CameraMovement.Instance.enabled = false; //desactivar movimiento de la camara de seguir al jugador
+
+        _player.transform.position = _roomSpawn.position; //mover al jugador, desactivar el movimiento y la animacion para evitar que entre en otra transicion
         PlayerAccess.Instance.MovementComponent.enabled= false;
         PlayerAccess.Instance.Animator.enabled= false;
-        _futureCamPos = new Vector3(_roomCameraPosition.position.x, _roomCameraPosition.position.y, _cameraTransform.position.z);
-        _onTransition = true;
+
+        _futureCamPos = new Vector3(_roomCameraPosition.position.x, _roomCameraPosition.position.y, _cameraTransform.position.z); //calculo posicion futura de la camara
+
         for (int i = 0; i < _transitions.Length; i++)
         {
-            _transitions[i].GetComponentInChildren<BoxCollider2D>().enabled = false;
+            _transitions[i].GetComponentInChildren<BoxCollider2D>().enabled = false; //se desactivan todas las transiciones
         }
+
+        _onTransition = true; //activar transicion
     }
 
-    private void CameraTransitionMovement()
+    private void OnTRansition()
     {
         if (_cameraTransform.position.x < _futureCamPos.x - 0.1
             || _cameraTransform.position.x > _futureCamPos.x + 0.1 || 
-            _cameraTransform.position.y < _futureCamPos.y - 0.1 || _cameraTransform.position.y > _futureCamPos.y + 0.1)
+            _cameraTransform.position.y < _futureCamPos.y - 0.1 || _cameraTransform.position.y > _futureCamPos.y + 0.1) //Mientras la camara no este en la posicion futura (con un intervalo porque con == no funciona)
         {
-            _cameraTransform.position = Vector3.Lerp(_cameraTransform.position, _futureCamPos, _transitionSpeed * Time.deltaTime);
+            _cameraTransform.position = Vector3.Lerp(_cameraTransform.position, _futureCamPos, _transitionSpeed * Time.deltaTime); //Se mueve la camara
         }
-        else
+        else //Cuando la camara ha llegado ha terminado la transicion, ahora proceso inverso de todo lo anterior
         {
             for (int i = 0; i < _transitions.Length; i++)
             {
-                _transitions[i].GetComponent<BoxCollider2D>().enabled = true;
+                _transitions[i].GetComponent<BoxCollider2D>().enabled = true; //Activar otra vez todas las transiciones
             }
-            PlayerAccess.Instance.MovementComponent.enabled = true;
+            PlayerAccess.Instance.MovementComponent.enabled = true; //Devolver al jugador movimiento y animaciones
             PlayerAccess.Instance.Animator.enabled = true;
-            CameraMovement.Instance.enabled = true;
-            _onTransition = false;
+
+            CameraMovement.Instance.enabled = true; //Hacer que la camara vuelva a seguir al jugador
+
+            _onTransition = false; //Se termina la transicion
         }
     }
     #endregion
@@ -61,15 +70,15 @@ public class RoomTransition : MonoBehaviour
     {
         _cameraTransform = CameraMovement.Instance.gameObject.transform;
         _player = PlayerAccess.Instance.transform;
-        _transitions = GameObject.FindGameObjectsWithTag("Transition");
+        _transitions = GameObject.FindGameObjectsWithTag("Transition"); //Ducktyping lo se pero ahorra mucho no me mateis
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_onTransition)
+        if (_onTransition) //si estas en una transicion
         {
-            CameraTransitionMovement();
+            OnTRansition();
         }
     }
 }
