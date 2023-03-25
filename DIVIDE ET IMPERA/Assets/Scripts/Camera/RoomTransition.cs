17789b9f1b0c162922a49dd90798710213a82a16
David-104 <118064911+David-104@users.noreply.github.com>
2023-03-25 20:30:21 +0100

    Transiciones de camara to wapas

--- DIVIDE ET IMPERA/Assets/Scripts/Camera/RoomTransition.cs
+++ DIVIDE ET IMPERA/Assets/Scripts/Camera/RoomTransition.cs
@@ -1,13 +1,15 @@
+using System.Xml.Serialization;
 using UnityEngine;
 
 public class RoomTransition : MonoBehaviour
 {
     #region References
     private Transform _cameraTransform;
+    private Transform _player;
     [SerializeField]
     private Transform _roomCameraPosition;
     [SerializeField]
-    private Transform _player;
+    private Transform _roomSpawn;
     #endregion
     #region Parameters
     [SerializeField]
@@ -15,24 +17,59 @@ public class RoomTransition : MonoBehaviour
     #endregion
     #region Properties
     private Vector3 _futureCamPos;
+    private bool _onTransition;
+    GameObject[] _transitions;
     #endregion
     #region Methods
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        _player.position = transform.position + new Vector3(1, 0, 0);
+        CameraMovement.Instance.enabled = false;
+        _player.transform.position = _roomSpawn.position;
+        PlayerAccess.Instance.MovementComponent.enabled= false;
+        PlayerAccess.Instance.Animator.enabled= false;
         _futureCamPos = new Vector3(_roomCameraPosition.position.x, _roomCameraPosition.position.y, _cameraTransform.position.z);
-        _cameraTransform.position = Vector3.Lerp(_cameraTransform.position, _futureCamPos, _transitionSpeed * Time.deltaTime);
+        _onTransition = true;
+        for (int i = 0; i < _transitions.Length; i++)
+        {
+            _transitions[i].GetComponentInChildren<BoxCollider2D>().enabled = false;
+        }
+    }
+
+    private void CameraTransitionMovement()
+    {
+        if (_cameraTransform.position.x < _futureCamPos.x - 0.1
+            || _cameraTransform.position.x > _futureCamPos.x + 0.1 || 
+            _cameraTransform.position.y < _futureCamPos.y - 0.1 || _cameraTransform.position.y > _futureCamPos.y + 0.1)
+        {
+            _cameraTransform.position = Vector3.Lerp(_cameraTransform.position, _futureCamPos, _transitionSpeed * Time.deltaTime);
+        }
+        else
+        {
+            for (int i = 0; i < _transitions.Length; i++)
+            {
+                _transitions[i].GetComponent<BoxCollider2D>().enabled = true;
+            }
+            PlayerAccess.Instance.MovementComponent.enabled = true;
+            PlayerAccess.Instance.Animator.enabled = true;
+            CameraMovement.Instance.enabled = true;
+            _onTransition = false;
+        }
     }
     #endregion
     // Start is called before the first frame update
     void Start()
     {
-        _cameraTransform = GameObject.Find("Main Camera").GetComponent<Transform>();
+        _cameraTransform = CameraMovement.Instance.gameObject.transform;
+        _player = PlayerAccess.Instance.transform;
+        _transitions = GameObject.FindGameObjectsWithTag("Transition");
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (_onTransition)
+        {
+            CameraTransitionMovement();
+        }
     }
 }
