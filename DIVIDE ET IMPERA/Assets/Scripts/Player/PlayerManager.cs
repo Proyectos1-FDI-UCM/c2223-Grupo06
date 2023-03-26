0068fcf9d89a0ac8c2b77b9333395618d8243e29
David-104 <118064911+David-104@users.noreply.github.com>
2023-03-26 20:12:18 +0200

    Partes del cuerpo sueltas ahora se elminan con el reset

--- DIVIDE ET IMPERA/Assets/Scripts/Player/PlayerManager.cs
+++ DIVIDE ET IMPERA/Assets/Scripts/Player/PlayerManager.cs
@@ -293,7 +293,7 @@ public class PlayerManager : MonoBehaviour
     {
         if (_brazos > 0) // si algún brazo y está en un espacio libre
         {
-            Instantiate(_brazoPrefab, _myTransform.position, _myTransform.rotation); // instanciación
+            Instantiate(_brazoPrefab, _myTransform.position, _myTransform.rotation, LevelManager.Instance.CurrentLevel.transform); // instanciación
             _brazos--; // un brazo menos
         }
     }
@@ -317,7 +317,7 @@ public class PlayerManager : MonoBehaviour
     {
         if (_piernas) // si tiene piernas 
         {
-            Instantiate(_piernaPrefab, _myTransform.position, _myTransform.rotation); // instanciación
+            Instantiate(_piernaPrefab, _myTransform.position, _myTransform.rotation, LevelManager.Instance.CurrentLevel.transform); // instanciación
             _piernas = false; // sin piernas
         }
     }
