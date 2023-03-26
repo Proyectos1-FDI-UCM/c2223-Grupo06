7af04f5ea68b12c88b6d973d006343b2a9ae5647
David-104 <118064911+David-104@users.noreply.github.com>
2023-03-26 01:01:44 +0100

    limpieza de codigo y comentarios

--- DIVIDE ET IMPERA/Assets/Scripts/Level/LevelManager.cs
+++ DIVIDE ET IMPERA/Assets/Scripts/Level/LevelManager.cs
@@ -8,68 +8,59 @@ public class LevelManager : MonoBehaviour
     #region References
     private static LevelManager _instance;
     public static LevelManager Instance { get { return _instance; } }
-
+    
     private GameObject _currentLevel;
-    public GameObject CurrentLevel { get { return _currentLevel; } }
+    public GameObject CurrentLevel { get { return _currentLevel; } } 
 
     [SerializeField]
-    private GameObject[] _levelsPrefabs;
+    private GameObject[] _levelsPrefabs; //array prefabs de niveles
     [SerializeField]
-    private GameObject[] _levels;
-    public GameObject[] Levels { get { return _levels; } }
+    private GameObject[] _levels; //array de niveles en la escena
+    [SerializeField] 
+    private Transform[] _roomSpawns; //array de spawns del jugador en cada sala (en la primera es el spawn original y
+                                     //en las demás coincide con los spanws de las transiciones)
     [SerializeField]
-    private Transform[] _roomSpawns;
-    public Transform[] RoomSpawns { get { return _roomSpawns; } }
-
-    private GameObject _player;
-    private Transform _roomSpawn;
-    [SerializeField]
-    private Transform _originalSpawn;
+    private Transform _originalSpawn; //spwan original del jugador
     #endregion
     #region Properties
-    private int _currentLevelNum;
-    public int CurrentLevelNum { get { return _currentLevelNum; } }
+    private int _currentLevelNum; //indice de la sala
+    private Transform _roomSpawn; //spawn actual
     #endregion
     #region Methods
-    public void IncrementLevelCounter()
+    public void IncrementLevelCounter() //aumenta indice y actualiza el nivel en el que te encuentras
     {
-        _currentLevelNum++;
+        _currentLevelNum++; 
         UpdateCurrentLevel();
     }
-    public void DecrementLevelCounter()
+    public void DecrementLevelCounter() //decrementa indice y actualiza el nivel en el que te encuentras
     {
         _currentLevelNum--;
         UpdateCurrentLevel();
     }
 
-    public void SetRoomSpawn(Transform roomSpawn)
-    {
-        _roomSpawn = roomSpawn;
-    }
-
     public void ResetCurrentLevel()
     {
-        ResetPlayer();
+        ResetPlayer(); //devuelve al jugador a las condiciones originales
 
-        Vector3 lvlTransform = _currentLevel.transform.position;
-        Destroy(_levels[_currentLevelNum]);
-        _levels[_currentLevelNum] = Instantiate(_levelsPrefabs[_currentLevelNum]);
-        _levels[_currentLevelNum].transform.position = lvlTransform;
+        Vector3 lvlTransform = _levels[_currentLevelNum].transform.position; //almacena posicion de la sala en la escena
+        Destroy(_levels[_currentLevelNum]); //destruye sala
+        _levels[_currentLevelNum] = Instantiate(_levelsPrefabs[_currentLevelNum]); //instancia la sala desde su prefab
+        _levels[_currentLevelNum].transform.position = lvlTransform; //mueve la sala a la posicion de la sala antes de resetearse
 
-        UpdateCurrentLevel();
+        UpdateCurrentLevel(); //actualiza nivel
     }
 
     private void ResetPlayer()
     {
-        PlayerManager.Instance.ChangePartInControl(_player);
-        _player.transform.parent = null;
-        _player.transform.position = _roomSpawn.position;
-        PlayerManager.Instance.RequestTimmyState(PlayerManager.TimmyStates.S0);
-        PlayerManager.Instance.EliminarObjeto();
-        PlayerAccess.Instance.BoneBar.ResetBar();
+        PlayerManager.Instance.ChangePartInControl(PlayerAccess.Instance.gameObject); //cambia el control al jugador en caso de estar controlando piernas
+        PlayerAccess.Instance.transform.parent = null; //adoptiont por si esta en plataforma
+        PlayerAccess.Instance.transform.position = _roomSpawn.position; //mueve player al spawn
+        PlayerManager.Instance.RequestTimmyState(PlayerManager.TimmyStates.S0); //devuelve al player al estado original
+        PlayerManager.Instance.EliminarObjeto(); //elimina objetos
+        PlayerAccess.Instance.BoneBar.ResetBar(); //elimina daño de caida acumulado
     }
 
-    private void UpdateCurrentLevel()
+    private void UpdateCurrentLevel() //actualiza spawn y nivel al del indice
     {
         _currentLevel = _levels[_currentLevelNum];
         _roomSpawn = _roomSpawns[_currentLevelNum];
@@ -84,7 +75,6 @@ public class LevelManager : MonoBehaviour
     {
         _currentLevelNum = 0;
         _roomSpawn = _originalSpawn;
-        _player = PlayerAccess.Instance.gameObject;
         UpdateCurrentLevel();
     }
 }
