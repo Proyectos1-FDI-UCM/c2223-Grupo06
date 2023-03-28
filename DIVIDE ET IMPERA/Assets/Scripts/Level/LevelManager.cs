7fb2e76a433e922d42983897f224f2c7109ed57e
David-104 <118064911+David-104@users.noreply.github.com>
2023-03-28 20:24:55 +0200

    Barra de salud y reseteo ahora son panas

--- DIVIDE ET IMPERA/Assets/Scripts/Level/LevelManager.cs
+++ DIVIDE ET IMPERA/Assets/Scripts/Level/LevelManager.cs
@@ -25,6 +25,7 @@ public class LevelManager : MonoBehaviour
     #region Properties
     private int _currentLevelNum; //indice de la sala
     private Transform _roomSpawn; //spawn actual
+    private float _currentHealth;
     #endregion
     #region Methods
     public void IncrementLevelCounter() //aumenta indice y actualiza el nivel en el que te encuentras
@@ -66,7 +67,7 @@ public class LevelManager : MonoBehaviour
         PlayerAccess.Instance.transform.position = _roomSpawn.position; //mueve player al spawn
         PlayerManager.Instance.RequestTimmyState(PlayerManager.TimmyStates.S0); //devuelve al player al estado original
         PlayerManager.Instance.EliminarObjeto(); //elimina objetos
-        PlayerAccess.Instance.BoneBar.ResetBar(); //elimina daño de caida acumulado
+        PlayerAccess.Instance.BoneBar.SetBar(_currentHealth); //elimina daño de caida acumulado
 
         PlayerAccess.Instance.InputController.ResetThisShit();
     }
@@ -97,6 +98,7 @@ public class LevelManager : MonoBehaviour
     {
         _currentLevel = _levels[_currentLevelNum];
         _roomSpawn = _roomSpawns[_currentLevelNum];
+        _currentHealth = PlayerAccess.Instance.BoneBar.CurrentBoneState;
     }
 
     public void GlobalReset()
@@ -110,6 +112,7 @@ public class LevelManager : MonoBehaviour
         }
 
         _roomSpawn = _originalSpawn;
+        _currentHealth = PlayerAccess.Instance.BoneBar.MaxBoneState;
         ResetPlayer();
 
         _currentLevelNum= 0;
