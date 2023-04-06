004178da5eb62586251dd5f1df063b2c855612ad
Inés <125393230+ineprimo@users.noreply.github.com>
2023-04-06 13:02:28 +0200

    MENU DE AJUSTES DE AUDIO IMPLEMENTADO
    (mis sueños se han hecho realidad, ahora es momento de romperlos)

--- DIVIDE ET IMPERA/Assets/Scripts/Sound/AudioManager.cs
+++ DIVIDE ET IMPERA/Assets/Scripts/Sound/AudioManager.cs
@@ -6,13 +6,30 @@ using UnityEngine.Audio;
 public class AudioManager : MonoBehaviour
 {
     [SerializeField]
-    public AudioMixer _bgmMixer;
+    private AudioMixer _bgmMixer;
+    [SerializeField]
+    private AudioMixer _sfxMixer;
+    [SerializeField]
+    private AudioMixer _ambienceMixer;
 
-    public void SetVolume(float _sliderValue)
+    public void SetBGMVolume(float _sliderValue)
     {
         // representa el valor del slider de manera logaritmica para que se haga bien la conversion; 
         // lo convierte en valores entre [-80, 0] pero en escala logaritmica
         _bgmMixer.SetFloat("BGMVolume", Mathf.Log10(_sliderValue) * 20);
+    }
+
+    public void SetSFXVolume(float _sliderValue)
+    {
+        // representa el valor del slider de manera logaritmica para que se haga bien la conversion; 
+        // lo convierte en valores entre [-80, 0] pero en escala logaritmica
+        _sfxMixer.SetFloat("SFXVolume", Mathf.Log10(_sliderValue) * 20);
+    }
+    public void SetAmbienceVolume(float _sliderValue)
+    {
+        // representa el valor del slider de manera logaritmica para que se haga bien la conversion; 
+        // lo convierte en valores entre [-80, 0] pero en escala logaritmica
+        _ambienceMixer.SetFloat("AmbienceMixer", Mathf.Log10(_sliderValue) * 20);
     }
 
 }
