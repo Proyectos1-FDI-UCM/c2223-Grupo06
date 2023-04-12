7e24d50e419dba3a98569e0ced7d8401dc864c8d
ariadnarc <ariadr02@ucm.es>
2023-04-12 20:24:59 +0200

    Menú puntuaciones

---
+++ DIVIDE ET IMPERA/Assets/Scripts/Score/Score.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    [SerializeField] private GameObject MALAMENTE;
+    [SerializeField] private GameObject REGULA;
+    [SerializeField] private GameObject ASEPTABLE;
+    [SerializeField] private GameObject CRANEOPERSENT;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(puntuacion.puntos < 500)
+        {
+            MALAMENTE.SetActive(true);
+            REGULA.SetActive(false);
+            ASEPTABLE.SetActive(false);
+            CRANEOPERSENT.SetActive(false);
+        }
+        else if(puntuacion.puntos >= 500 || puntuacion.puntos < 600)
+        {
+            MALAMENTE.SetActive(false);
+            REGULA.SetActive(true);
+            ASEPTABLE.SetActive(false);
+            CRANEOPERSENT.SetActive(false);
+        }
+        else if (puntuacion.puntos >= 600 || puntuacion.puntos < 900)
+        {
+            MALAMENTE.SetActive(false);
+            REGULA.SetActive(false);
+            ASEPTABLE.SetActive(true);
+            CRANEOPERSENT.SetActive(false);
+        }
+        else if (puntuacion.puntos >= 900 || puntuacion.puntos < 1000)
+        {
+            MALAMENTE.SetActive(false);
+            REGULA.SetActive(false);
+            ASEPTABLE.SetActive(false);
+            CRANEOPERSENT.SetActive(true);
+        }
+    }
+}
