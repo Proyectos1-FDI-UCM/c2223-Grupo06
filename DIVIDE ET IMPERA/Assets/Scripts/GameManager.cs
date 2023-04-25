97fd9074c023036cf32e0494864f7dd06a3e1cdc
cynthia <118978961+cyntrist@users.noreply.github.com>
2023-04-25 17:56:55 +0200

    endings!!!!!!!!!!!!!!!!!!!!!

--- DIVIDE ET IMPERA/Assets/Scripts/GameManager.cs
+++ DIVIDE ET IMPERA/Assets/Scripts/GameManager.cs
@@ -70,7 +70,7 @@ public class GameManager : MonoBehaviour
             case GameStates.GAMEOVER:                    //     *FIN DEL JUEGO* -> PUNTUACION
                 if (_tiempo > 500)
                     SubScore(100);
-                else if (_tiempo <= 500 && _tiempo > 400)
+                else if (_tiempo > 400)
                     SubScore(50);
                 else if (_tiempo <= 400 && _tiempo > 300)
                     SubScore(25);
@@ -84,11 +84,11 @@ public class GameManager : MonoBehaviour
                 break;
             case GameStates.SCORE:                      //     *PUNTUACIÓN*
                 int ending = 0;
-                if (_score >= 500 && _score < 600)
+                if (_score >= 500 && _score < 700)
                     ending = 1;
-                else if (_score >= 600 && _score < 900)
+                else if (_score >= 700 && _score < 1000)
                     ending = 2;
-                else if (_score >= 900 && _score < 1000)
+                else if (_score >= 1000)
                     ending = 3;
                 if (_UIManager != null) _UIManager.ScoreMenuSetUp(_score, ending);
                 break;
